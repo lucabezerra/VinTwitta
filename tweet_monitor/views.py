f93e86cc60ea56880a916778f1be8afb9855df54
Luca Bezerra <luca@pool.ai>
2017-06-03 14:03:43 -0300

    Added REST endpoints for retrieving filtered tweets.

--- tweet_monitor/views.py
+++ tweet_monitor/views.py
@@ -9,20 +9,75 @@ from django.urls import reverse
 from rest_framework import generics
 from social_django.models import UserSocialAuth
 import tweepy
+from tweepy import TweepError
 
 from .models import Tweet, Hashtag
 from .serializers import TweetSerializer
 
 
+# ########### Retrieval Endpoints ########### #
+
+
 class TweetsView(generics.ListAPIView):
-    """
-    Returns a list of all tweets.
-    """
+    """ Returns a list of all tweets. """
     model = Tweet
     serializer_class = TweetSerializer
 
     def get_queryset(self):
         return Tweet.objects.all()
+
+
+class UserTweetsView(generics.ListAPIView):
+    """ Returns a list of all tweets from a specific user. """
+    model = Tweet
+    serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('username'):
+            return Tweet.objects.filter(owner__iexact=self.kwargs.get('username'))
+
+
+class DateRangeTweetsView(generics.ListAPIView):
+    """ Returns a list of all tweets in a specific date range. """
+    model = Tweet
+    serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('date'):
+            return Tweet.objects.filter(creation_date__gte=self.kwargs.get('date'))
+
+
+class TextTweetsView(generics.ListAPIView):
+    """ Returns a list of all tweets containing a specific text. """
+    model = Tweet
+    serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('text'):
+            return Tweet.objects.filter(text__icontains=self.kwargs.get('text'))
+
+
+class HashtagTweetsView(generics.ListAPIView):
+    """ Returns a list of all tweets containing a specific hashtag. """
+    model = Tweet
+    serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        hashtag = self.kwargs.get('hashtag')
+        if hashtag:
+            hashtag = "#" + hashtag if hashtag[0] != "#" else hashtag
+            return Tweet.objects.filter(Q(hashtags__name__iexact=hashtag))
+
+
+# ########### Creation Endpoints ########### #
+class FetchTweetsView(generics.ListCreateAPIView):
+    """ Fetches 200 most recent tweets from a specific user and stores them in the DB. """
+    model = Tweet
+    serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('username'):
+            return Tweet.objects.filter(owner__iexact=self.kwargs.get('username'))
 
 
 @login_required
@@ -51,21 +106,28 @@ def add_handle(request):
                 tweepy_handler = generate_tweepy_handler(user_obj.extra_data['access_token']['oauth_token'],
                                                          user_obj.extra_data['access_token']['oauth_token_secret'])
 
-                statuses = tweepy_handler.user_timeline(screen_name=handle, count=200)
-                tweets_list = []
+                try:
+                    statuses = tweepy_handler.user_timeline(screen_name=handle, count=200)
+                    tweets_list = []
 
-                for status in statuses:
-                    tweet = Tweet(provider_id=status.id, owner=status.user.screen_name,
-                                  creation_date=status.created_at, text=status.text)
-                    tweets_list.append(tweet)
+                    for status in statuses:
+                        tweet = Tweet(provider_id=status.id, owner=status.user.screen_name, text=status.text,
+                                      creation_date=status.created_at)
+                        tweets_list.append(tweet)
 
-                Tweet.objects.bulk_create(tweets_list)
+                    Tweet.objects.bulk_create(tweets_list)
 
-                saved_tweets = Tweet.objects.filter(owner__iexact=handle)
-                for t in saved_tweets:
-                    t.extract_hashtags()
+                    saved_tweets = Tweet.objects.filter(owner__iexact=handle)
+                    for t in saved_tweets:
+                        t.extract_hashtags()
 
-                messages.success(request, "The handle {} was added!".format(handle))
+                    messages.success(request, "The handle {} was added!".format(handle))
+                except TweepError as err:
+                    if err.response.status_code == 404:
+                        messages.error(request, "The provided Twitter handle doesn't exist. Please check the spelling.")
+                    else:
+                        messages.error(request, "There was a problem in the request, please check the handle spelling "
+                                                "and try again.")
         else:
             messages.error(request, "Please provide a Twitter handle to have its tweets fetched.")
     else:
